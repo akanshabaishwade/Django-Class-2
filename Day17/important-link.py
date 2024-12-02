5c1095ba2571744e2cbb5f6c32b974be3a8b698e
akanshabaishwade <akanshabaishwade@gmail.com>
2024-12-02 22:48:58 +0530

    [Add] complete django course

--- Day17/important-link.py
+++ Day17/important-link.py
@@ -1,3 +1,4 @@
 # https://dbdiagram.io/home -database schema
 # https://app.diagrams.net/ draw.io 
 # https://lucid.app/lucidspark/
+# https://docs.github.com/en/account-and-profile/setting-up-and-managing-your-github-profile/customizing-your-profile/managing-your-profile-readme
